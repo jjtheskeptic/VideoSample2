c95585178f82354706120cca37f565775bc672a4
jjtheskeptic <jj_in_atlanta@yahoo.com>
2023-01-08 11:20:24 -0500

    Mostly working!

--- pi_surveillance.py
+++ pi_surveillance.py
@@ -25,12 +25,6 @@ import http.client, urllib.request, urllib.parse, urllib.error, base64
 #Python 3.11.1
 
 
-# construct the argument parser and parse the arguments
-#ap = argparse.ArgumentParser()
-#ap.add_argument("-c", "--conf", required=True, help="path to the JSON configuration file")
-
-#args = vars(ap.parse_args())
-
 # filter warnings, load the configuration and initialize the Dropbox
 # client
 warnings.filterwarnings("ignore")
@@ -56,14 +50,7 @@ if conf["use_dropbox"]:
     client = dropbox.Dropbox(conf["dropbox_access_token"])
     print("[SUCCESS] dropbox account linked")
 videoDeviceNumber=conf["videoDeviceNumber"]
-
-	
-# ===============initialize the camera and grab a reference to the raw camera capture
-#camera = PiCamera()
-#camera.resolution = tuple(conf["resolution"])
-#camera.framerate = conf["fps"]
-#rawCapture = PiRGBArray(camera, size=tuple(conf["resolution"]))
-#vs=VideoStream(src=0).start()  #This works for built-in camera on new laptop
+filenameDateFormatString="%Y_%m_%d_%H_%M_%S"
 
 vs=VideoStream(src=videoDeviceNumber).start()   # this works for external USB camera on new laptop: 0:internal; 2:external USB
 #on the pi, deviceNumber0 works for the external USB camera
@@ -143,7 +130,7 @@ while True: #for f in camera.capture_continuous(rawCapture, format="bgr", use_vi
 			# check to see if the number of frames with consistent motion is
 			# high enough
 			if motionCounter >= conf["min_motion_frames"]:
-				print("[INFO] - MOTION DETECTED") 
+				print("[INFO] - MOTION DETECTED-"+datetime.datetime.now().strftime("%A %d %B %Y %I_%M_%S%p")) 
                 # write the image to temporary file
 				t = TempImage()
 				cv2.imwrite(t.path, frame)
@@ -157,24 +144,23 @@ while True: #for f in camera.capture_continuous(rawCapture, format="bgr", use_vi
 						data = f.read()
 					try:
 						hasCat=False
-						print("[INFO] making POST request")
+						print("[INFO] making POST request-",datetime.datetime.now().strftime("%A %d %B %Y %I_%M_%S%p"))
 						conn = http.client.HTTPSConnection('cattraps.cognitiveservices.azure.com')
 						apiCallCounter+=1
 						conn.request("POST", "/computervision/imageanalysis:analyze?api-version=2022-10-12-preview&%s" % params, data, headers)
-						print("[INFO] POST complete")
+						print("[INFO] POST complete-",datetime.datetime.now().strftime("%A %d %B %Y %I_%M_%S%p"))
 						response = conn.getresponse()
 						responseData = response.read()
 						azureEndTime = datetime.datetime.now()
 						azureSeconds=(azureEndTime-azureStartTime).seconds
-						print("[INFO]:upload time in seconds",azureSeconds)
+						print("[INFO]:upload time in seconds",azureSeconds,datetime.datetime.now().strftime("%A %d %B %Y %I_%M_%S%p"))
 						conn.close()		
 						pythonObj=json.loads(responseData)
-						print(pythonObj["tagsResult"])
+						#print(pythonObj["tagsResult"])
 						tags=pythonObj["tagsResult"]
 						for object in tags["values"]:
 							if hasCat==True:
 								break
-							#print(object["name"], object["confidence"])
 							if object["name"] in catTags:
 								if object["confidence"] > .6:
 									print(object["name"], object["confidence"])
@@ -183,12 +169,15 @@ while True: #for f in camera.capture_continuous(rawCapture, format="bgr", use_vi
 										
 						
 						print ("numCalls:",apiCallCounter)
+						#t.cleanup() #delete the temp image
 						if hasCat == False:  #Delete the temp image if no cat detected
 							#if 1==2:
 							t.cleanup()
 						else:
-							print("capture video here")
-							captureStartTime = datetime.datetime.now()							
+							print("capture video here",datetime.datetime.now().strftime("%A %d %B %Y %I_%M_%S%p"))
+							captureStartTime = datetime.datetime.now()		
+							startTimeString = captureStartTime.strftime("%A %d %B %Y %I_%M_%S%p")	
+							videoFileName="./ImageFiles/Capture_"+datetime.datetime.now().strftime(filenameDateFormatString)+".avi"			
 							# #### Here is where I need to record a few seconds of video
 							# #try this: https://www.etutorialspoint.com/index.php/320-how-to-capture-a-video-in-python-opencv-and-save
 							#The codec's seem to be here - not sure if needed or not: https://github.com/cisco/openh264/releases
@@ -203,26 +192,21 @@ while True: #for f in camera.capture_continuous(rawCapture, format="bgr", use_vi
 							# frame_width = int(cap.get(3))
 							# frame_height = int(cap.get(4))
 							
-							print("100")
+							print("100",datetime.datetime.now().strftime("%A %d %B %Y %I_%M_%S%p"))
 							#video_output=cv2.VideoWriter('captured_video.avi',cv2.VideoWriter_fourcc('X', 'V', 'I', 'D'),20, (frame_width,frame_height))
-							video_output=cv2.VideoWriter('captured_video.avi',cv2.VideoWriter_fourcc('X', 'V', 'I', 'D'),20, tuple(conf["resolution"]))
-							print ("200")
-							while ((1==1) and ((datetime.datetime.now()-captureStartTime).seconds < 2) ):
-								print ("300.")
-								#ret,frame=cap.read()
-								#ret,frame_raw=vs.read()
-								#if ret==True:
-								print("400")
+							#video_output=cv2.VideoWriter('./ImageFiles/captured_video.avi',cv2.VideoWriter_fourcc('X', 'V', 'I', 'D'),20, tuple(conf["resolution"]))
+							video_output=cv2.VideoWriter(videoFileName,cv2.VideoWriter_fourcc('X', 'V', 'I', 'D'),20, tuple(conf["resolution"]))
+							print ("200"+datetime.datetime.now().strftime("%A %d %B %Y %I_%M_%S%p"))
+							while ((1==1) and ((datetime.datetime.now()-captureStartTime).seconds < 3) ):
+								frame_raw=vs.read() 
 								video_output.write(frame_raw)
-								cv2.imshow('frame',frame_raw)
-								#print("capturing video..")
+								#cv2.imshow('frame',frame_raw)  # this is too slow to come up on the RasberryPi/VNC								
 								#else:
 								#	break
 								#cap.release()
 							# video_output.release()
-							print("Video capture stopped")
-							# # cv2.destroyAllWindows() #probably don't want to do this
-							# # cv2.destroyWindow(video_output)
+							print("Video capture stopped",datetime.datetime.now().strftime("%A %d %B %Y %I_%M_%S%p"))
+
 							
 
 	#### END AZURE
@@ -263,7 +247,7 @@ while True: #for f in camera.capture_continuous(rawCapture, format="bgr", use_vi
 	# check to see if the frames should be displayed to screen
 	if conf["show_video"]:
 		# display the security feed
-		cv2.imshow("Security Feed", frame)
+		cv2.imshow("Cat-Cam", frame)
 		key = cv2.waitKey(1) & 0xFF
 
 		# if the `q` key is pressed, break from the lop
--- pyimagesearch/tempimage.py
+++ pyimagesearch/tempimage.py
@@ -1,12 +1,17 @@
 # import the necessary packages
 import uuid
 import os
+import datetime
 
 class TempImage:
 	def __init__(self, basePath="./ImageFiles", ext=".jpg"):
 		# construct the file path
+		filenameDateFormatString="%Y_%m_%d_%H_%M_%S"
+		timeStr=datetime.datetime.now().strftime(filenameDateFormatString)
+		#self.path = "{base_path}/{rand}{ext}".format(base_path=basePath,
+		#	rand=str(uuid.uuid4()), ext=ext)
 		self.path = "{base_path}/{rand}{ext}".format(base_path=basePath,
-			rand=str(uuid.uuid4()), ext=ext)
+			rand=timeStr, ext=ext)
             
 	def cleanup(self):
 		# remove the file
